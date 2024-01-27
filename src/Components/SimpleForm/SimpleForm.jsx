04dc187d2f87e2c2c33e8622fa9740a5c346603d
GGTuran <tayyabturan67@gmail.com>
2024-01-27 12:04:42 +0600

    handling

--- src/Components/SimpleForm/SimpleForm.jsx
+++ src/Components/SimpleForm/SimpleForm.jsx
@@ -1,12 +1,22 @@
 
 
 const SimpleForm = () => {
+    const handleSubmit = e =>{
+        e.preventDefault();
+        console.log(e.target.name.value);
+        console.log(e.target.email.value);
+        console.log(e.target.phone.value);
+    }
     return (
         <div>
-            <form >
-                <input type="text" />
+            <form  onSubmit={handleSubmit}>
+                <input type="text" name="name" />
                 <br />
-                <button>Submit</button>
+                <input type="email" name="email" id="" />
+                <br />
+                <input type="text" name="phone" id="" />
+                <br />
+                <input type="submit" value="Submit" />
             </form>
         </div>
     );
