589cbd6f05cfbfb46f55122131e0387335a68489
GGTuran <tayyabturan67@gmail.com>
2024-01-28 12:21:45 +0600

    reusuing

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,37 @@
 
 import './App.css'
+import ReusableForm from './Components/ReusableForm/ReusableForm'
 // import Hookform from './Components/Hookform/Hookform'
 // import Hookform from './Components/Hookform/Hookform'
 // import RefForm from './Components/RefForm/RefForm'
 // import StatefulForm from './Components/StatefulForm/StatefulForm'
-import SimpleForm from './Components/SimpleForm/SimpleForm'
+// import SimpleForm from './Components/SimpleForm/SimpleForm'
 
 function App() {
- 
+
+  const handleSignUpSubmit = data =>{
+
+    console.log(data)
+   
+}
+const handleUpdateProfile = data =>{
+
+  console.log(data)
+
+}
 
   return (
     <>
       
       <h1>Forming</h1>
-      <SimpleForm></SimpleForm>
+      {/* <SimpleForm></SimpleForm> */}
       {/* <StatefulForm></StatefulForm> */}
       {/* <RefForm></RefForm> */}
       {/* <Hookform></Hookform> */}
       {/* <Hookform></Hookform> */}
+
+      <ReusableForm formTitle={'Sign up'} handleSubmit={handleSignUpSubmit}></ReusableForm>
+      <ReusableForm formTitle={'Update it!!'} handleSubmit={handleUpdateProfile} submitBtnText='Update'></ReusableForm>
       
     </>
   )
