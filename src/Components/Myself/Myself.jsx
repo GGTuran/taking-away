8a6551ea8bfdefe97d5400156813c4ba29377080
GGTuran <tayyabturan67@gmail.com>
2024-01-29 10:09:18 +0600

    special one

--- src/Components/Myself/Myself.jsx
+++ src/Components/Myself/Myself.jsx
@@ -1,9 +1,13 @@
+import Special from "../Special/Special";
 
 
 const Myself = () => {
     return (
         <div>
            <h2>Myself</h2> 
+           <section className="flex">
+            <Special></Special>
+           </section>
         </div>
     );
 };
