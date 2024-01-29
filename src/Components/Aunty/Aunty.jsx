52a3b64811a138fdd45eb4e1c05b45112bb5f0e2
GGTuran <tayyabturan67@gmail.com>
2024-01-29 10:05:37 +0600

    compoing

--- src/Components/Aunty/Aunty.jsx
+++ src/Components/Aunty/Aunty.jsx
@@ -1,9 +1,14 @@
+import Cousin from "../Cousin/Cousin";
 
 
 const Aunty = () => {
     return (
         <div>
             <h2>Aunty</h2>
+            <section className="flex">
+                <Cousin name={'Mamshad'}></Cousin>
+                <Cousin name={'Mamuna'}> </Cousin>
+            </section>
         </div>
     );
 };
