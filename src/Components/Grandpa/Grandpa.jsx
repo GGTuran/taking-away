0d294a245ee073ba651ff0aacb45c9162993a378
GGTuran <tayyabturan67@gmail.com>
2024-01-30 19:31:11 +0600

    used context with function

--- src/Components/Grandpa/Grandpa.jsx
+++ src/Components/Grandpa/Grandpa.jsx
@@ -1,23 +1,27 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import Aunty from "../Aunty/Aunty";
 import Dad from "../Dad/Dad";
 import Uncle from "../Uncle/Uncle";
 import './Grandpa.css'
 
 export const AssetContext = createContext('Is my Queen');
+export const MoneyContext = createContext(1000);
 
 const Grandpa = () => {
+    const [money, setMoney] = useState(1000);
     const asset = 'Diamond';
     return (
         <div className="grandpa     ">
             <h2>Grandpa</h2>
-            <AssetContext.Provider value="Is my Queen">
-                <section className="flex">
-                    <Dad asset={asset}></Dad>
-                    <Uncle></Uncle>
-                    <Aunty></Aunty>
-                </section>
-            </AssetContext.Provider>
+            <MoneyContext.Provider value={[money, setMoney]}>
+                <AssetContext.Provider value="Is my Queen">
+                    <section className="flex">
+                        <Dad asset={asset}></Dad>
+                        <Uncle></Uncle>
+                        <Aunty></Aunty>
+                    </section>
+                </AssetContext.Provider>
+            </MoneyContext.Provider>
         </div>
     );
 };
--- src/Components/Myself/Myself.jsx
+++ src/Components/Myself/Myself.jsx
@@ -1,13 +1,19 @@
+import { useContext } from "react";
 import Special from "../Special/Special";
 import PropTypes from 'prop-types';
+import { MoneyContext } from "../Grandpa/Grandpa";
 
 const Myself = ({asset}) => {
+    const[money, setMoney] = useContext(MoneyContext);
     return (
         <div>
            <h2>Myself</h2> 
            <section className="flex">
             <Special asset={asset}></Special>
+            
            </section>
+           <p>Net money:{money}</p>
+           <button onClick={() => setMoney(money+1000)} > Add money!!</button>
         </div>
     );
 };
