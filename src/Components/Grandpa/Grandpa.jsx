ec8ff914393545b3b75474ec3b7fc71de25ed5ca
GGTuran <tayyabturan67@gmail.com>
2024-01-30 19:16:01 +0600

    contexting

--- src/Components/Grandpa/Grandpa.jsx
+++ src/Components/Grandpa/Grandpa.jsx
@@ -1,18 +1,23 @@
+import { createContext } from "react";
 import Aunty from "../Aunty/Aunty";
 import Dad from "../Dad/Dad";
 import Uncle from "../Uncle/Uncle";
 import './Grandpa.css'
+
+const AssetContext = createContext('Is my Queen');
 
 const Grandpa = () => {
     const asset = 'Diamond';
     return (
         <div className="grandpa     ">
             <h2>Grandpa</h2>
-           <section className="flex">
-             <Dad asset={asset}></Dad>
-            <Uncle></Uncle>
-            <Aunty></Aunty>
-           </section>
+            <AssetContext.Provider value="Is my Queen">
+                <section className="flex">
+                    <Dad asset={asset}></Dad>
+                    <Uncle></Uncle>
+                    <Aunty></Aunty>
+                </section>
+            </AssetContext.Provider>
         </div>
     );
 };
