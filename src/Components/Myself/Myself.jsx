4fd1de1d5ad1446750d4748f639ece50a1ac06f8
GGTuran <tayyabturan67@gmail.com>
2024-01-31 16:12:45 +0600

    as it was

--- src/Components/Myself/Myself.jsx
+++ src/Components/Myself/Myself.jsx
@@ -12,7 +12,7 @@ const Myself = ({asset}) => {
             <Special asset={asset}></Special>
             
            </section>
-           <h1>boy he is gonna</h1>
+           
            
            <p>Net money:{money}</p>
            <button onClick={() => setMoney(money+1000)} > Add money!!</button>
