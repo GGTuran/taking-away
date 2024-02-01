6342a27e966199bb498548f889beaf7eac2b4d62
GGTuran <tayyabturan67@gmail.com>
2024-02-01 10:09:20 +0600

    nai kichu

--- src/Components/Special/Special.jsx
+++ src/Components/Special/Special.jsx
@@ -9,7 +9,7 @@ const Special = ({asset}) => {
             <h2>Special person  {queen}</h2>
             <p>Has {asset}</p>
             
-            <p>bar ache</p>
+            
         </div>
     );
 };
