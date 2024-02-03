1b87fe6d418485e22391fa832eb74d4510205369
GGTuran <tayyabturan67@gmail.com>
2024-02-03 09:58:55 +0600

    using the context api

--- src/Components/Special/Special.jsx
+++ src/Components/Special/Special.jsx
@@ -1,13 +1,16 @@
 import PropTypes from 'prop-types' ;
 import { useContext } from 'react';
-import { AssetContext } from '../Grandpa/Grandpa';
+import { AssetContext, MoneyContext } from '../Grandpa/Grandpa';
 
 const Special = ({asset}) => {
     const queen = useContext(AssetContext);
+    const [money, setMoney] = useContext(MoneyContext);
+
     return (
         <div>
             <h2>Special person  {queen}</h2>
             <p>Has {asset}</p>
+            <p>Has money:{money}</p>
             
             
             
