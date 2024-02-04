0b84bdd87d4f98b3310f5111d92f8cc338d2a670
GGTuran <tayyabturan67@gmail.com>
2024-02-04 20:10:56 +0600

    context api

--- src/Components/Cousin/Cousin.jsx
+++ src/Components/Cousin/Cousin.jsx
@@ -1,10 +1,14 @@
+import { useContext } from "react";
+import { MoneyContext } from "../Grandpa/Grandpa";
 
 
 const Cousin = ({name}) => {
+    const[money ,setMoney] = useContext(MoneyContext);
     return (
         <div>
             <h2>Cousin</h2>
             <p>{name}</p>
+            <p>does not have {money}</p>
            
             
             
