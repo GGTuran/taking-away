f85a7889b57a894945100b47b6421ef30243475a
GGTuran <tayyabturan67@gmail.com>
2024-02-05 21:16:04 +0600

    set mooney

--- src/Components/Brother/Brother.jsx
+++ src/Components/Brother/Brother.jsx
@@ -1,13 +1,15 @@
 import { useContext } from "react";
-import { AssetContext } from "../Grandpa/Grandpa";
+import { AssetContext, MoneyContext } from "../Grandpa/Grandpa";
 
 
 const Brother = () => {
     const money = useContext(AssetContext)
+    const [mopney , setMoney] = useContext(MoneyContext)
     return (
         <div>
             <h2>Brother</h2>
             <p>does not have {money}</p>
+            <button>money:{setMoney}</button>
         </div>
     );
 };
