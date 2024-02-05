b48ec8691e3bb92758b91f41c31489f2ed0ff4d7
GGTuran <tayyabturan67@gmail.com>
2024-02-05 21:14:56 +0600

    money

--- src/Components/Brother/Brother.jsx
+++ src/Components/Brother/Brother.jsx
@@ -1,9 +1,13 @@
+import { useContext } from "react";
+import { AssetContext } from "../Grandpa/Grandpa";
 
 
 const Brother = () => {
+    const money = useContext(AssetContext)
     return (
         <div>
             <h2>Brother</h2>
+            <p>does not have {money}</p>
         </div>
     );
 };
